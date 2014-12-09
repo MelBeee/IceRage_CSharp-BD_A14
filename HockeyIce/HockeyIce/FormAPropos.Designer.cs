a4218d80c4ce98d0b39ab1a4936da41908a59f16
MelBeee <mel.beee@deepteal.com>
2014-12-09 10:14:31 -0500

    :end:  FORM A PROPOS FINITO, PAS TOUCHE

--- HockeyIce/HockeyIce/FormAPropos.Designer.cs
+++ HockeyIce/HockeyIce/FormAPropos.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAPropos));
             this.LB_Text = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
@@ -39,6 +40,7 @@
             this.LB_Description = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.FB_Quitter = new FlashButton.FlashButton();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -49,9 +51,9 @@
             this.LB_Text.AutoSize = true;
             this.LB_Text.Font = new System.Drawing.Font("Kristen ITC", 12F);
             this.LB_Text.ForeColor = System.Drawing.Color.White;
-            this.LB_Text.Location = new System.Drawing.Point(33, 2);
+            this.LB_Text.Location = new System.Drawing.Point(33, 5);
             this.LB_Text.Name = "LB_Text";
-            this.LB_Text.Size = new System.Drawing.Size(104, 28);
+            this.LB_Text.Size = new System.Drawing.Size(83, 23);
             this.LB_Text.TabIndex = 9;
             this.LB_Text.Text = "À Propos";
             this.LB_Text.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LB_Text_MouseDown);
@@ -68,30 +70,31 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.FB_Continuer);
             this.panel1.Controls.Add(this.LB_Description);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Location = new System.Drawing.Point(3, 31);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(547, 530);
+            this.panel1.Size = new System.Drawing.Size(434, 369);
             this.panel1.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(123, 214);
+            this.label4.Location = new System.Drawing.Point(86, 165);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(214, 60);
+            this.label4.Size = new System.Drawing.Size(166, 48);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Ouvrir fenêtre À Propos\r\nQuitter le programme\r\nQuitter le programme\r\nAfficher le " +
-    "classement";
+            this.label4.Text = "Ouvrir fenêtre À Propos\r\nQuitter le programme\r\nQuitter le programme\r\nPropriété de" +
+    "s couleurs";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(36, 214);
+            this.label3.Location = new System.Drawing.Point(19, 165);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 60);
+            this.label3.Size = new System.Drawing.Size(68, 48);
             this.label3.TabIndex = 7;
             this.label3.Text = "F1 - \r\nCtrl+Q - \r\nEscape - \r\nF2 - ";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -100,9 +103,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(55, 191);
+            this.label2.Location = new System.Drawing.Point(30, 143);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(256, 20);
+            this.label2.Size = new System.Drawing.Size(217, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "Raccourcis Claviers";
             // 
@@ -112,14 +115,14 @@
             this.label1.Font = new System.Drawing.Font("Kristen ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(7, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 42);
+            this.label1.Size = new System.Drawing.Size(124, 33);
             this.label1.TabIndex = 5;
             this.label1.Text = "Ice Rage";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::HockeyIce.Properties.Resources.Hockey_Stick_Pin;
-            this.pictureBox1.Location = new System.Drawing.Point(341, 266);
+            this.pictureBox1.Location = new System.Drawing.Point(229, 98);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(203, 262);
@@ -135,7 +138,7 @@
             this.FB_Continuer.ImageDisable = global::HockeyIce.Properties.Resources.FermerDisable;
             this.FB_Continuer.ImageNeutral = global::HockeyIce.Properties.Resources.FermerNormal;
             this.FB_Continuer.ImageOver = global::HockeyIce.Properties.Resources.FermerHover;
-            this.FB_Continuer.Location = new System.Drawing.Point(13, 497);
+            this.FB_Continuer.Location = new System.Drawing.Point(11, 337);
             this.FB_Continuer.Margin = new System.Windows.Forms.Padding(5);
             this.FB_Continuer.Name = "FB_Continuer";
             this.FB_Continuer.Size = new System.Drawing.Size(115, 23);
@@ -146,9 +149,9 @@
             // 
             this.LB_Description.AutoSize = true;
             this.LB_Description.Font = new System.Drawing.Font("Kristen ITC", 9F);
-            this.LB_Description.Location = new System.Drawing.Point(11, 48);
+            this.LB_Description.Location = new System.Drawing.Point(137, 16);
             this.LB_Description.Name = "LB_Description";
-            this.LB_Description.Size = new System.Drawing.Size(343, 22);
+            this.LB_Description.Size = new System.Drawing.Size(275, 17);
             this.LB_Description.TabIndex = 1;
             this.LB_Description.Text = "Créé par Melissa Boucher et Xavier Brosseau";
             this.LB_Description.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -172,18 +175,27 @@
             this.FB_Quitter.ImageDisable = global::HockeyIce.Properties.Resources.CloseDisable;
             this.FB_Quitter.ImageNeutral = global::HockeyIce.Properties.Resources.CloseNormal;
             this.FB_Quitter.ImageOver = global::HockeyIce.Properties.Resources.CloseHover;
-            this.FB_Quitter.Location = new System.Drawing.Point(523, 4);
+            this.FB_Quitter.Location = new System.Drawing.Point(410, 3);
             this.FB_Quitter.Margin = new System.Windows.Forms.Padding(5);
             this.FB_Quitter.Name = "FB_Quitter";
             this.FB_Quitter.Size = new System.Drawing.Size(25, 25);
             this.FB_Quitter.TabIndex = 7;
             this.FB_Quitter.Click += new System.EventHandler(this.FB_Quitter_Click);
             // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(414, 75);
+            this.label5.TabIndex = 9;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
             // FormAPropos
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(163)))), ((int)(((byte)(220)))));
-            this.ClientSize = new System.Drawing.Size(552, 564);
+            this.ClientSize = new System.Drawing.Size(440, 404);
             this.Controls.Add(this.LB_Text);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.FB_Quitter);
@@ -219,5 +231,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
     }
 }
