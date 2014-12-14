b48fe2433a18bc085d05413bcb143fdcc79ad7f0
Brosseau <xavierb.2006@hotmail.com>
2014-12-14 16:59:27 -0500

    That Commit

--- FichiersSQL/CommandesUtilisées.sql
+++ FichiersSQL/CommandesUtilisées.sql
@@ -51,20 +51,94 @@ inner join EQUIPES e on e.NUMEQUIPE = cj.NUMEQUIPE
 where point >=0;
 
 --Remplir combobox Division
-select NOM from divisions
+select NOM from divisions;
 
 --Classement Équipe selon Division
 select e.LOGO, ce.NOM, e.VILLE, e.DATEINTRODUCTION, d.NOM, ce.POINTS
 from ClassementEquipe ce
 inner join equipes e on e.NUMEQUIPE = ce.NUMEQUIPE
 inner join divisions d on d.NUMDIVISION = ce.NUMDIVISION
-where d.NOM = 'Est'
+where d.NOM = 'Est';
 
 --Gestion
 --/////////////////////////////////////////////////////////////////////////////////////
+--Remplir CB Gestion Equipe
+select numequipe, nom 
+from equipes;
 
+--Remplir CB Gestion Division
+select numdivision, nom 
+from divisions;
 
+--Gestion Ajout Match
+insert into matchs
+(NUMMATCH, NUMEQUIPEVIS, NUMEQUIPEMAI, DATEHEURE, HEURE, LIEU, POINTAGEMAISON, POINTAGEVISITEUR) values
+(:NumMatch,:NumEquipeVis,:NumEquipeMai,:DateHeure,:Heure, :Lieu,:PointageMaison, :PointageVisiteur);
+            
+--Modifier Match
+update matchs set nummatch = :NumMatch,
+numequipevis = :NumEquipeVis,
+numequipemai = :NumEquipeMai,
+dateheure = :DateHeure,
+heure = :Heure,
+lieu = :Lieu,
+pointagemaison = :PointageMaison,
+pointagevisiteur = :PointageVisiteur;
 
+--Load Info Match
+select ev.nom, em.nom, dateheure, heure, lieu, pointagemaison, pointagevisiteur, numequipevis, numequipemai
+from matchs m
+inner join EQUIPES ev on ev.NUMEQUIPE = m.NUMEQUIPEVIS
+inner join EQUIPES em on em.NUMEQUIPE = m.NUMEQUIPEMAI;
 
+-- Ajouter équipe
+insert into EQUIPES
+(NUMEQUIPE, NOM, LOGO, VILLE, NUMDIVISION, DATEINTRODUCTION) values
+(:Numequipe,:nom,:Logo,:Ville,:NumDivision,:DateValue);
 
+-- Modifier équipe
+update EQUIPES set
+NOM=:nom, VILLE=:Ville,
+NUMDIVISION=:NumDivision,
+DATEINTRODUCTION=:DateValue;
 
+-- Modifier LogoEquipe
+update EQUIPES set 
+Logo=:Logo;
+
+--Load Info équipe
+select e.*, d.nom from equipes e 
+inner join divisions d on d.numdivision = e.numdivision;
+
+--Ajout Division
+insert into divisions
+(NUMDIVISION,    NOM,    DATECREATION) values
+(:NumDivision,   :Nom,   :DateValue);
+
+--Modifier Division
+update divisions set
+numdivision = :NumDivision,
+NOM = :Nom,
+DATECREATION = :DateValue;
+
+--Load InfoDivision
+select * from divisions;
+
+--Ajout Joueur
+insert into joueurs
+(NUMJOUEUR,  NOM,    PRENOM,     NAISSANCE,  NUMEROMAILLOT,  TYPEJOUEUR,     PHOTO,  NUMEQUIPE) values (:NumJoueur, :Nom,   :Prenom,    :Naissance, :NumMaillot,    :TypeJoueur,    :Photo, :NumEquipe)";
+
+--Modifier Joueur
+update joueurs set
+NUMJOUEUR =      :NumJoueur,
+NOM =            :Nom,
+PRENOM =         :Prenom,
+NAISSANCE =      :Naissance,
+NUMEROMAILLOT =  :NumMaillot,
+TYPEJOUEUR =     :TypeJoueur,
+PHOTO =          :Photo,
+NUMEQUIPE =      :NumEquipe;
+
+--Load Info Joueur
+select j.*, e.nom from joueurs j
+inner join equipes e on e.NUMEQUIPE = j.numequipe 
