dd60cc31b4bce1664a835b81d0b08862d2f07cbe
Brosseau <xavierb.2006@hotmail.com>
2014-12-15 09:31:53 -0500

    dfs

--- HockeyIce/HockeyIce/FormAPropos.Designer.cs
+++ HockeyIce/HockeyIce/FormAPropos.Designer.cs
@@ -53,7 +53,7 @@
             this.LB_Text.ForeColor = System.Drawing.Color.White;
             this.LB_Text.Location = new System.Drawing.Point(33, 3);
             this.LB_Text.Name = "LB_Text";
-            this.LB_Text.Size = new System.Drawing.Size(100, 27);
+            this.LB_Text.Size = new System.Drawing.Size(80, 22);
             this.LB_Text.TabIndex = 9;
             this.LB_Text.Text = "À Propos";
             this.LB_Text.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormAPropos_MouseDown);
@@ -83,7 +83,7 @@
             this.label4.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(100, 196);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(214, 75);
+            this.label4.Size = new System.Drawing.Size(166, 60);
             this.label4.TabIndex = 8;
             this.label4.Text = "Ouvrir fenêtre À Propos\r\nQuitter le programme\r\nQuitter le programme\r\nNaviguer Jou" +
     "eurs\r\nNaviguer Joueurs";
@@ -94,7 +94,7 @@
             this.label3.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(19, 196);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 75);
+            this.label3.Size = new System.Drawing.Size(61, 60);
             this.label3.TabIndex = 7;
             this.label3.Text = "F1 -\r\nCtrl+Q -\r\nEscape -\r\n< -\r\n> -";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -105,7 +105,7 @@
             this.label2.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(32, 172);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(256, 20);
+            this.label2.Size = new System.Drawing.Size(217, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "Raccourcis Claviers";
             // 
@@ -115,7 +115,7 @@
             this.label1.Font = new System.Drawing.Font("Kristen ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(7, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 42);
+            this.label1.Size = new System.Drawing.Size(124, 33);
             this.label1.TabIndex = 5;
             this.label1.Text = "Ice Rage";
             // 
@@ -151,7 +151,7 @@
             this.LB_Description.Font = new System.Drawing.Font("Kristen ITC", 9F);
             this.LB_Description.Location = new System.Drawing.Point(157, 22);
             this.LB_Description.Name = "LB_Description";
-            this.LB_Description.Size = new System.Drawing.Size(343, 22);
+            this.LB_Description.Size = new System.Drawing.Size(275, 17);
             this.LB_Description.TabIndex = 1;
             this.LB_Description.Text = "Créé par Melissa Boucher et Xavier Brosseau";
             this.LB_Description.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -196,7 +196,7 @@
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(163)))), ((int)(((byte)(220)))));
-            this.ClientSize = new System.Drawing.Size(515, 473);
+            this.ClientSize = new System.Drawing.Size(515, 474);
             this.Controls.Add(this.LB_Text);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.FB_Quitter);
